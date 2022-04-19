e1fec5dac14dbb2870b513b42335db4a3954680e
xodus05 <82927278+xodus05@users.noreply.github.com>
2022-04-19 20:03:02 +0900

    라디오버튼에 따라 이미지변경2

--- app/src/main/java/kr/hs/emirims2121/mirim_project_0414_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirims2121/mirim_project_0414_1/MainActivity.java
@@ -10,10 +10,11 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
     RadioGroup rg;
-    CheckBox checkStart;
+    Switch checkStart;
     ImageView imgv;
     LinearLayout linearSub;
 
@@ -24,17 +25,18 @@ public class MainActivity extends AppCompatActivity {
         linearSub = findViewById(R.id.Linear_sub);
 
         rg = findViewById(R.id.rg);
+        rg.setOnCheckedChangeListener(rgListener);
         checkStart = findViewById(R.id.check_start);
         imgv = findViewById(R.id.imgv);
-        Button btnDone = findViewById(R.id.btn_done);
-        btnDone.setOnClickListener(btnListener);
+//        Button btnDone = findViewById(R.id.btn_done);
+//        btnDone.setOnClickListener(btnListener);
 
         checkStart.setOnCheckedChangeListener(checkListener);
     }
-    View.OnClickListener btnListener = new View.OnClickListener() {
+    RadioGroup.OnCheckedChangeListener rgListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
-        public void onClick(View view) {
-            switch (rg.getCheckedRadioButtonId()) {
+        public void onCheckedChanged(RadioGroup radioGroup, int selectedId) {
+            switch (selectedId) {
                 case R.id.rb_dog:
                     imgv.setImageResource(R.drawable.choihyunwook);
                     break;
@@ -47,6 +49,22 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+//    View.OnClickListener btnListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            switch (rg.getCheckedRadioButtonId()) {
+//                case R.id.rb_dog:
+//                    imgv.setImageResource(R.drawable.choihyunwook);
+//                    break;
+//                case R.id.rb_cat:
+//                    imgv.setImageResource(R.drawable.choihyunwookd);
+//                    break;
+//                case R.id.rb_rabbit:
+//                    imgv.setImageResource(R.drawable.choihyunwookrabbit);
+//                    break;
+//            }
+//        }
+//    };
     CompoundButton.OnCheckedChangeListener checkListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
